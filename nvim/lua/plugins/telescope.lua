25125ff7c863b4d4abd14b26f9be139ab82d48e9
LongYinStudio <1606776851@qq.com>
2023-07-27 16:37:55 +0800

    进一步完善nvim-telescope的配置

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,4 +1,9 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+local status, actions = pcall(require, "telescope.actions")
+if (not status) then
+  return
+end
 
 -- 进入telescope页面会是插入模式，回到正常模式就可以用j和k来移动了
 
@@ -6,3 +11,15 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})  -- 环境里要安装ripgrep
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      n = {
+        ["q"] = actions.close,
+        ["l"] = actions.file_edit
+      }
+    },
+    file_ignore_patterns = {"./node_modules"}
+  },
+}
