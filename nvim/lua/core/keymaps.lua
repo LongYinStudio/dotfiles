7f37baf16c4c4a8578b81e906993b9690c6441b4
LongYinStudio <1606776851@qq.com>
2023-08-14 08:45:43 +0800

    v 1行，上下5行

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = ";"
 
+local opts = { noremap = true, silent = true }
+
 local keymap = vim.keymap.set
 
 -- ---------- 插入模式 ---------- ---
@@ -9,6 +11,11 @@ keymap("i", "jk", "<ESC>")
 -- 单行或多行移动
 keymap("v", "J", ":m '>+1<CR>gv=gv")
 keymap("v", "K", ":m '<-2<CR>gv=gv")
+-- v 1行，上下5行
+keymap('v', 'K', '5k', opts)
+keymap('v', 'J', '5j', opts)
+keymap('v', 'H', '0', opts)
+keymap('v', 'L', '$', opts)
 
 -- ---------- 正常模式 ---------- ---
 -- 窗口
