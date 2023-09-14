1e9e8d32e7b3002ceb24996845e6f955c0098fea
LongYinStudio <1606776851@qq.com>
2023-09-14 21:45:42 +0800

    新增cd alias

--- zsh/.my-zsh/aliases.zsh
+++ zsh/.my-zsh/aliases.zsh
@@ -1,2 +1,9 @@
 # 设置别名(可以覆盖其他的)，run "alias"可以查看系统，插件，主题等等所有别名
 alias zshconfig="vim ~/.zshrc"
+
+# cd
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~"
