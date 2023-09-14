edf81b70aebadc7c1276f4b3ed2b7a2df2bd677a
LongYinStudio <1606776851@qq.com>
2023-09-14 21:31:54 +0800

    优化env.zsh

--- zsh/.my-zsh/env.zsh
+++ zsh/.my-zsh/env.zsh
@@ -1,7 +1,8 @@
-export PATH=$PATH:/home/longyinstudio/dev-tools/node-v18.13.0-linux-x64/bin
+# node 使用的nvm
+bin="/home/longyinstudio/bin"
+go="/home/longyinstudio/dev-tools/go/bin"
 export JAVA_HOME=/home/longyinstudio/dev-tools/jdk1.8.0_351
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
-export PATH=$JAVA_HOME/bin:$PATH                                  
 export JRE_HOME=$JAVA_HOME/jre 
-export PATH=/home/longyinstudio/bin:$PATH   
-export PATH=$PATH:/home/longyinstudio/dev-tools/go/bin 
+
+export PATH="$node:$JAVA_HOME/bin:$bin:$go:$PATH"
