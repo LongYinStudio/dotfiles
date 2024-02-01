1308c8d37681094a5d946644ab59d8f9dd1353a0
LongYinStudio <1606776851@qq.com>
2024-02-01 18:13:04 +0800

    toggleterm换成horizontal方向

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,5 +3,5 @@ require("toggleterm").setup({
   -- 打开新终端后自动进入插入模式
   start_in_insert = true,
   -- 打开方式
-  direction = "float" --float/vertical/tab
+  direction = "horizontal" --float/vertical/tab
 })
