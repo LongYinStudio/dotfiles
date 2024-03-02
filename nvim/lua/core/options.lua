753ad9b1109cbe95d103bd54a7300a574c9dd4d7
LongYinStudio <1606776851@qq.com>
2024-03-02 16:21:59 +0800

    修改options.lua

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -4,29 +4,22 @@ local opt = vim.opt
 opt.relativenumber = true
 opt.number = true
 
--- 设置'modifiable'选项为true
-opt.modifiable = true
-
 -- 缩进
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 
--- 补全增强
-opt.wildmenu = true
+opt.wildmenu = true -- 补全增强
+opt.modifiable = true -- 设置'modifiable'选项为true
+opt.cursorline = true -- 光标行
+opt.mouse:append("a") -- 启用鼠标
+opt.clipboard:append("unnamedplus") -- 系统剪贴板
+-- opt.undofile = true --持久化撤销
 
--- 防止包裹
+-- 自动换行
 opt.wrap = false
-
--- 光标行
-opt.cursorline = true
-
--- 启用鼠标
-opt.mouse:append("a")
-
--- 系统剪贴板
-opt.clipboard:append("unnamedplus")
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = "*.md, *.txt", command = "setlocal wrap" })
 
 -- 默认新窗口右和下
 opt.splitright = true
@@ -39,4 +32,4 @@ opt.smartcase = true
 -- 外观
 opt.termguicolors = true
 opt.signcolumn = "yes"
-vim.cmd[[colorscheme onedark]]
+vim.cmd([[colorscheme onedark]])
