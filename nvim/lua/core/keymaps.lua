ba49a4092b0d0b31dcd16183d8f1cba6514d9e88
LongYinStudio <1606776851@qq.com>
2024-03-09 16:21:29 +0800

    重新配置“移动”快捷键

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -7,13 +7,18 @@ local keymap = vim.keymap.set
 -- 配置删除(d)不复制
 keymap({ "n", "v" }, "d", '"_d')
 
+-- 单行或多行移动
+keymap("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+keymap("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+keymap("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+keymap("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+keymap("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+keymap("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
 -- ---------- 插入模式 ---------- ---
 keymap("i", "jk", "<ESC>")
 
 -- ---------- 视觉模式 ---------- ---
--- 单行或多行移动
-keymap("v", "J", ":m '>+1<CR>gv=gv")
-keymap("v", "K", ":m '<-2<CR>gv=gv")
 -- H一行开始 L一行结束
 keymap("v", "H", "0", opts)
 keymap("v", "L", "$", opts)
