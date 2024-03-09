7a3a5c28e7e1250ebfa947b98a72de1b2fe4c724
LongYinStudio <1606776851@qq.com>
2024-03-09 14:43:12 +0800

    配置nvim-tree git状态显示

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -3,7 +3,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
-	git = { enable = true }, --显示git状态
+	git = { enable = true },
 	-- 完全禁止内置netrw
 	disable_netrw = true,
 	update_focused_file = {
@@ -13,7 +13,7 @@ require("nvim-tree").setup({
 	sort_by = "case_sensitive",
 	renderer = {
 		root_folder_label = false, -- 隐藏当前根目录地址显示
-		highlight_git = false,
+		highlight_git = true,
 		highlight_opened_files = "none",
 		indent_markers = {
 			enable = true,
@@ -23,7 +23,7 @@ require("nvim-tree").setup({
 				file = true,
 				folder = true,
 				folder_arrow = true,
-				git = true,
+				git = false,
 			},
 			glyphs = {
 				folder = {
@@ -40,7 +40,45 @@ require("nvim-tree").setup({
 	filters = {
 		-- 隐藏 .文件(H进行切换)
 		dotfiles = false,
+		git_ignored = false,
 		-- 隐藏 node_modules 文件夹
 		custom = { "node_modules" },
 	},
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
 })
+
+-- dark主题下
+vim.cmd([[
+  highlight NvimTreeLspDiagnosticsError guifg=#c63629
+  highlight NvimTreeLspDiagnosticsWarning guifg=yellow
+
+  highlight NvimTreeGitNew guifg=#81b88b
+  highlight NvimTreeGitDirty guifg=#E2C08D
+  highlight NvimTreeGitDeleted guifg=#c74e39
+  highlight NvimTreeGitRenamed guifg=#73C991
+  highlight NvimTreeGitStaged guifg=#E2C08D
+  highlight NvimTreeGitIgnored guifg=#8C8C8C
+  highlight NvimTreeGitMerge guifg=#e4676b
+]])
+
+-- light主题下
+-- vim.cmd([[
+--   highlight NvimTreeLspDiagnosticsError guifg=#c63629
+--   highlight NvimTreeLspDiagnosticsWarning guifg=yellow
+--
+--   highlight NvimTreeGitNew guifg=#587c0c
+--   highlight NvimTreeGitDirty guifg=#895503
+--   highlight NvimTreeGitDeleted guifg=#ad0707
+--   highlight NvimTreeGitRenamed guifg=#007100
+--   highlight NvimTreeGitStaged guifg=#895503
+--   highlight NvimTreeGitIgnored guifg=#ad0707
+--   highlight NvimTreeGitMerge guifg=#e4676b
+-- ]])
