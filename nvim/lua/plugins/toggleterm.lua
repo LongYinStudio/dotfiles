e26866149df906adcf97550729682ae5fff31f61
LongYinStudio <1606776851@qq.com>
2024-06-04 22:59:26 +0800

    fix(neovim):整理toggleterm快捷键

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,6 @@ return {
 		"akinsho/toggleterm.nvim",
 		config = function()
 			require("toggleterm").setup({
-				open_mapping = [[<leader>`]],
 				-- 打开新终端后自动进入插入模式
 				start_in_insert = true,
 				-- 打开方式
