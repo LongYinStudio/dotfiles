1d0a6d1679c03b7c404348dae88527652ae88e40
LongYinStudio <1606776851@qq.com>
2024-07-11 20:34:14 +0800

    feat(neovim):新增插件nvim-lsp-file-operations

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -75,4 +75,14 @@ return {
 			})
 		end,
 	},
+	{
+		"antosha417/nvim-lsp-file-operations", -- 自动修改引用（如：修改vue组件名）
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-tree.lua",
+		},
+		config = function()
+			require("lsp-file-operations").setup()
+		end,
+	},
 }
