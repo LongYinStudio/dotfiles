435546ffad7cbd16bc9652c3d77df8e3eefdc799
LongYinStudio <1606776851@qq.com>
2024-07-14 19:40:30 +0800

    fix(neovim):更改lualine配置

--- README.md
+++ README.md
@@ -8,18 +8,21 @@
 
    - 安装oh-my-zsh：参照官方github文档，将它安装在$HOME/.oh-my-zsh
    - 复制 zsh/.zshrc 到 ~/
-   -  安装 `zsh-syntax-highlighting`：执行 `git clone https://gitee.com/Annihilater/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting`
-   -  安装 `zsh-autosuggestions`：执行 `git clone https://gitee.com/phpxxo/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions`
+   - 安装 `zsh-syntax-highlighting`：执行 `git clone https://gitee.com/Annihilater/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting`
+   - 安装 `zsh-autosuggestions`：执行 `git clone https://gitee.com/phpxxo/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions`
    - 主题
 
      1. 自制主题(zsh/my.zsh-theme)放在\~/.oh-my-zsh/custom/themes 或者\~/.oh-my-zsh/themes 都行
      2. 推荐主题 [romkatv/powerlevel10k: A Zsh theme (github.com)](https://github.com/romkatv/powerlevel10k)
+
    - 最后 `source ~/.zshrc`
+
 2. vim
 
    - 下载.vimrc 和.vim 放在~里，github 搜索安装 `vim-plug`
    - onedark 主题配置：autoload/onedark.vim 放在\~/.vim/autoload 里，colors/onedark.vim 放在\~/.vim/colors 里
    - 插件安装：在.vim 里用 `:PlugInstall`
+
 3. neovim
 
    - 安装 apt / brew / scoop 等等包管理器直接可以安装，详细参考官网（[Home - Neovim](https://neovim.io/)）
@@ -28,6 +31,7 @@
      1. neovim \~/.config/nvim/lua/plugins/plugins-setup.lua
      2. normal 模式下:w 就可以自动安装配置的插件 || :Lazy 呼出 Lazy 管理界面
    - 安装\~/.config/nvim/lua/plugins/conform.lua 里用到的 formatters
+
 4. tmux
    - 用包管理器安装tmux
    - 复制 tmux/.tmux.conf 到 ~/
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,3 +1,22 @@
+local icons = {
+	SemiCircleLeft = "",
+	SemiCircleRight = "",
+	Formatter = "󰉢",
+	LSP = "",
+	FileSize = "",
+	Location = "",
+	Left = " ",
+	Right = "",
+	Branch = "",
+	File = "",
+	diagnostics = {
+		Error = "",
+		Warning = "",
+		Hint = "󰌵",
+		Info = "",
+	},
+}
+
 return {
 	{
 		"nvim-lualine/lualine.nvim", -- 状态栏
@@ -5,45 +24,82 @@ return {
 		config = function()
 			require("lualine").setup({
 				options = {
-					theme = "onedark",
+					theme = "auto",
+					globalstatus = true,
 					component_separators = {
 						left = "|",
 						right = "|",
 					},
-					-- https://github.com/ryanoasis/powerline-extra-symbols
-					section_separators = {
-						left = " ",
-						right = "",
-					},
-					globalstatus = true,
+					disabled_filetypes = { "alpha", "dashboard", "Outline" },
+					always_divide_middle = true,
 				},
 				extensions = { "nvim-tree" },
 				sections = {
+					lualine_a = {
+						"mode",
+					},
+					lualine_b = {
+						{ "filename", icon = icons.File },
+						{ "branch", icon = icons.Branch },
+					},
 					lualine_c = {
-						"filename",
 						{
-							"lsp_progress",
-							spinner_symbols = { " ", " ", " ", " ", " ", " " },
+							"diagnostics",
+							sources = {
+								-- "nvim_diagnostic",
+								"nvim_lsp",
+							},
+							sections = {
+								"info",
+								"error",
+								"warn",
+								"hint",
+							},
+							diagnostic_color = {
+								error = { fg = "DiaganosticError" },
+								warn = { fg = "DiagnosticWarn" },
+								info = { fg = "DiaganosticInfo" },
+								hint = { fg = "DiaganosticHint" },
+							},
+							colored = true,
+							update_in_insert = true,
+							always_visible = true,
+							symbols = icons.diagnostic,
 						},
 					},
 					lualine_x = {
-						"filesize",
 						{
 							"fileformat",
 							symbols = {
-								unix = "", -- e712
-								dos = "", -- e70f
-								mac = "", -- e711
+								unix = " - LF", -- e712
+								dos = " - CRLF", -- e70f
+								mac = " - CR", -- e711
 							},
-							-- symbols = {
-							-- 	unix = "LF",
-							-- 	dos = "CRLF",
-							-- 	mac = "CR",
-							-- },
+							separator = { left = icons.SemiCircleLeft, right = icons.SemiCircleRight },
+							color = { bg = "#B4BEFE", fg = "#eeeeee", gui = "bold" },
 						},
-						"encoding",
-						"filetype",
+						{
+							"filesize",
+							icon = icons.FileSize,
+						},
 					},
+					lualine_y = { "filetype", "encoding" },
+					lualine_z = {
+						"progress",
+						{
+							"location",
+							icon = icons.Location,
+							color = { gui = "bold" },
+						},
+					},
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
 				},
 			})
 		end,
