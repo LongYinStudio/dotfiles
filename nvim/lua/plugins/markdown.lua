e7800737a148b34d93e036f62561499744ed3aba
LongYinStudio <1606776851@qq.com>
2024-07-24 17:12:37 +0800

    feat(neovim):新增markdown实时渲染插件

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -7,4 +7,28 @@ return {
 			vim.fn["mkdp#util#install"]()
 		end,
 	},
+	{
+		"MeanderingProgrammer/markdown.nvim",
+		main = "render-markdown",
+		ft = { "markdown" },
+		opts = {
+			heading = {
+				-- default:
+				-- signs = { "󰫎 " },
+				-- icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+
+				-- icons = { "󰫎 " },
+				icons = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
+				signs = { "#1", "#2", "#3", "#4", "#5", "#6" },
+				-- signs = { "━ " },
+			},
+			pipe_table = {
+				border = { "╭", "┬", "╮", "├", "┼", "┤", "╰", "┴", "╯", "│", "─" },
+			},
+		},
+		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+		-- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	},
 }
