04746c77bfdf90ee61f74b8d455021551c60fe46
LongYinStudio <1606776851@qq.com>
2024-07-31 12:48:34 +0800

    feat(neovim): lualine增加git diff

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -66,6 +66,20 @@ return {
 							always_visible = true,
 							symbols = icons.diagnostic,
 						},
+						{
+							"diff",
+							symbols = { added = " ", modified = " ", removed = " " },
+							source = function()
+								local gitsigns = vim.b.gitsigns_status_dict
+								if gitsigns then
+									return {
+										added = gitsigns.added,
+										modified = gitsigns.changed,
+										removed = gitsigns.removed,
+									}
+								end
+							end,
+						},
 					},
 					lualine_x = {
 						{
