50a32a04f8c6b2d5a1f098b128311a9606e5dd0f
LongYinStudio <1606776851@qq.com>
2024-08-20 13:08:30 +0800

    fix: 剪贴板相关

---
+++ i3/dep.md
@@ -0,0 +1,11 @@
+1. 输入法: fcitx5-im fcitx5-chinese-addons fcitx5-pinyin-zhwiki (主题: https://github.com/catppuccin/fcitx5)
+2. 剪贴板: xclip
+3. 壁纸: feh
+4. 通知: dunst
+5. 透明: picom
+6. 网络托盘: nm-applet
+7. 截图: flameshot
+8. 终端: alacritty、kitty、wezterm
+9. others:
+   - i3/i3-gaps、i3status、polybar、rofi(自行编译支持中文的)
+   - 自定义外观: lxappearance
--- nvim/lua/others/wsl.lua
+++ nvim/lua/others/wsl.lua
@@ -1,6 +1,11 @@
-vim.cmd [[
-  augroup Yank
-  autocmd!
-  autocmd TextYankPost * :call system('/mnt/c/windows/system32/clip.exe ',@")
-  augroup END
-]]
+vim.cmd([[
+  if system('uname -r') =~ "microsoft"
+      let s:clip = '/mnt/c/Windows/System32/clip.exe'  " default location
+      if executable(s:clip)
+          augroup WSLYank
+              autocmd!
+              autocmd TextYankPost * call system('echo '.shellescape(join(v:event.regcontents, "\<CR>")).' | '.s:clip)
+          augroup END
+      end
+  end
+]])
